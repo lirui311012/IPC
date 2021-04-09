e423dea34091fabd4c43abe87944deeb330c9081
ubuntu-lirui <lirui311012@163.com>
2021-04-09 16:11:38 +0800

    pipe

--- pipe.cpp
+++ pipe.cpp
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
@@ -10,15 +11,40 @@
 #include <stdio.h>
 using namespace std;
 
+
+#define MAX_BUFF 1024
+
 void server(int readfd,int writefd)
 {
-    
+    int fd;
+    ssize_t n;
+    char buff[MAX_BUFF+1];
+    if((n = read(readfd,buff,MAX_BUFF)) == 0)
+    {
+        cout << "end-of-file while reading pathname" << endl;
+    }
+    buff[n] = '\0';
+    //open file
+    if((fd = open(buff,O_RDONLY)) < 0 )
+    {
+        //open file failed
+        cout << "open file failed" << endl;
+        snprintf(buff+n,sizeof(buff)-n,":can't open, %s\n",strerror(errno));
+        n = strlen(buff);
+        write(writefd,buff,n);
+    }else{
+        //open file success
+        while((n = read(fd,buff,MAX_BUFF)) > 0){
+            write(writefd,(const void *)buff,n);
+        }
+        close(fd);
+    }
 }
 void client(int readfd,int writefd)
 {
     size_t len;
-    char buff[1024];
-    fgets(buff,1024,stdin);
+    char buff[MAX_BUFF];
+    fgets(buff,MAX_BUFF,stdin);
     len = strlen((const char *)buff);
     if(buff[len-1] == '\n')
     {
@@ -26,7 +52,7 @@ void client(int readfd,int writefd)
     }
     write(writefd,(const void *)buff,len);
     ssize_t readlen;
-    while((readlen = read(readfd,buff,1024)) > 0){
+    while((readlen = read(readfd,buff,MAX_BUFF)) > 0){
         write(STDOUT_FILENO,(const void *)buff,readlen);
     }
 }
@@ -48,18 +74,18 @@ int main(int argc, char **argv)
     __pid_t pid = fork();
     if (pid == 0)
     {
-        //子进程
+        //son
         cout << "sub" << endl;
         close(pipe1[1]);
         close(pipe2[0]);
         server(pipe1[0],pipe2[1]);
         exit(0);
     }
-    //父进程
+    //father
     cout << "father" << endl;
     close(pipe1[0]);
     close(pipe2[1]);
-    server(pipe2[0],pipe1[1]);    
+    client(pipe2[0],pipe1[1]);    
     waitpid(pid,NULL,0);
 
     return 0;
